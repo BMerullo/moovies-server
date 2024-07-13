0a4cccaee4055f84c1d66d41a820b145049637ce
Bob Merullo <bmerullo85@gmail.com>
2024-07-12 20:36:44 -0600

    change to routes

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
   app.get("/api/movie", movieController.findAllMovies)
   app.post("/api/movie", authenticate, movieController.createNewMovie)
   app.get("/api/user/movie/:userId", movieController.findAllMoviesByUser)
-  app.get("/api/movie/:objectId", movieController.findOneMovie)
+  app.get("/api/movie/:movieId/:createdBy", movieController.findOneMovie)
   app.put("/api/movie/:objectid", movieController.updateMovie)
   app.delete("/api/movie/:id", movieController.deleteMovie)
 }
